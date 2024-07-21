33922e2f2e6b0b06f2e4a337f89f74ecf8cced02
Jackson Huang <jacksonh201325@gmail.com>
2024-07-21 13:09:25 +0200

    setup base lazy vim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,54 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- LazyVim auto format
+vim.g.autoformat = true
+
+-- LazyVim picker to use.
+-- Can be one of: telescope, fzf
+-- Leave it to "auto" to automatically use the picker
+-- enabled with `:LazyExtras`
+vim.g.lazyvim_picker = "auto"
+
+-- LazyVim root dir detection
+-- Each entry can be:
+-- * the name of a detector function like `lsp` or `cwd`
+-- * a pattern or array of patterns like `.git` or `lua`.
+-- * a function with signature `function(buf) -> string|string[]`
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+
+-- LazyVim automatically configures lazygit:
+--  * theme, based on the active colorscheme.
+--  * editorPreset to nvim-remote
+--  * enables nerd font icons
+-- Set to false to disable.
+vim.g.lazygit_config = true
+
+-- Options for the LazyVim statuscolumn
+vim.g.lazyvim_statuscolumn = {
+  folds_open = false,  -- show fold sign when fold is open
+  folds_githl = false, -- highlight fold sign with git sign color
+}
+
+-- Optionally setup the terminal to use
+-- This sets `vim.o.shell` and does some additional configuration for:
+-- * pwsh
+-- * powershell
+-- LazyVim.terminal.setup("pwsh")
+
+-- Hide deprecation warnings
+vim.g.deprecation_warnings = false
+
+-- Set filetype to `bigfile` for files larger than 1.5 MB
+-- Only vim syntax will be enabled (with the correct filetype)
+-- LSP, treesitter and other ft plugins will be disabled.
+-- mini.animate will also be disabled.
+vim.g.bigfile_size = 1024 * 1024 * 1.5 -- 1.5 MB
+
+-- Show the current document symbols location from Trouble in lualine
+-- You can disable this for a buffer by setting `vim.b.trouble_lualine = false`
+vim.g.trouble_lualine = true
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,7 +14,7 @@ opt.smartindent = true -- Insert indents automatically
 opt.expandtab = true
 
 -- line wrapping
-opt.wrap = false -- Disable line wrap
+opt.wrap = true -- Disable line wrap
 
 -- search settings
 opt.ignorecase = true -- Ignore case
@@ -22,6 +22,7 @@ opt.smartcase = true  -- Don't ignore case with capitals
 
 -- cursor line
 opt.cursorline = true -- Enable highlighting of the current line
+
 
 -- appeareance
 opt.termguicolors = true -- True color support
@@ -31,34 +32,40 @@ opt.background = "dark"
 opt.backspace = "indent,eol,start"
 
 opt.autowrite = true
-opt.clipboard = "unnamedplus"
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
 -- opt.cmdheight = 0            -- More space for displaying messages
 opt.completeopt = "menu,menuone,noselect"
-opt.conceallevel = 3         -- Hide * markup for bold and italic
-opt.confirm = true           -- Confirm to save changes before exiting modified buffer
-opt.formatoptions = "joqlnt" -- tcq
+opt.conceallevel = 3 -- Hide * markup for bold and italic
+opt.confirm = true   -- Confirm to save changes before exiting modified buffer
+opt.foldlevel = 99
+opt.foldlevelstart = 99
+opt.formatoptions = "jcroqlnt" -- tcqj
 opt.grepformat = "%f:%l:%c:%m"
 opt.grepprg = "rg --vimgrep"
 opt.inccommand = "nosplit" -- preview incremental substitute
-opt.laststatus = 0
-opt.foldlevelstart = 99
-opt.mouse = "a"    -- Enable muse mode
-opt.pumblend = 10  -- Popup blend
-opt.pumheight = 10 -- Maximum number of entries in a popup
-opt.scrolloff = 4  -- Lines of contex
-opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
-opt.shortmess:append({ W = true, I = true, c = true })
+opt.mouse = "a"            -- Enable muse mode
+opt.pumblend = 10          -- Popup blend
+opt.pumheight = 10         -- Maximum number of entries in a popup
+opt.scrolloff = 4          -- Lines of contex
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+opt.shortmess:append({ W = true, I = true, c = true, C = true })
 opt.showcmd = true
-opt.showmode = false   -- Dont show mode since we have a statusline
+opt.showmode = false                          -- Dont show mode since we have a statusline
 opt.sidescrolloff = 4
-opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+opt.signcolumn = "yes"                        -- Always show the signcolumn, otherwise it would shift the text each time
 opt.spelllang = { "en" }
-opt.splitbelow = true  -- Put new windows below current
-opt.splitright = true  -- Put new windows right of current
-opt.timeoutlen = 300
+opt.splitbelow = true                         -- Put new windows below current
+opt.splitright = true                         -- Put new windows right of current
+opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
 opt.undofile = true
 opt.undolevels = 10000
 opt.updatetime = 200               -- Save swap file and trigger CursorHold
 opt.wildmode = "longest:full,full" -- Command-line completion mode
-opt.winbar = "%=%m %f"
+-- opt.winbar = "%=%m %f"
 opt.winminwidth = 10               -- Minimum window width
+
+opt.jumpoptions = "view"
+opt.laststatus = 3 -- global statusline
+opt.spelloptions:append("noplainbuffer")
+opt.splitkeep = "screen"
+opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
